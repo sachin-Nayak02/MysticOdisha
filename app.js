793fb3af4835574a45a7df6edf1112edcf71a6b8
Sadasiba Nayak <nayaksadasiba02@gmail.com>
2025-01-15 20:27:44 +0530

    successfully create delete and edit button

--- app.js
+++ app.js
@@ -46,11 +46,35 @@ app.post("/mysticOdisha",async(req,res,next)=>{
     res.redirect("/addDb")
 })
 
-app.get("/place/:id/edit",(req,res,next)=>{
+//update
+app.get("/place/:id/edit",async(req,res,next)=>{
     let {id}=req.params;
-    res.render("editDb.ejs")
+    let result=await Place.findById(id);
+    res.render("editDb.ejs",{result})
+})
+app.put("/mysticOdisha/:id",async(req,res,next)=>{
+    let {id}=req.params;
+    let {name,description,image,imagebn,dist,type,location,bio}=req.body;
+    await Place.findByIdAndUpdate(id,{name:name})
+    await Place.findByIdAndUpdate(id,{description:description})
+    await Place.findByIdAndUpdate(id,{image:image})
+    await Place.findByIdAndUpdate(id,{imagebn:imagebn})
+    await Place.findByIdAndUpdate(id,{dist:dist})
+    await Place.findByIdAndUpdate(id,{type:type})
+    await Place.findByIdAndUpdate(id,{location:location})
+    await Place.findByIdAndUpdate(id,{bio:bio})
+
+    console.log("success");
+    res.redirect("/")
+  
 })
 
+//Delete
+app.delete("/place/:id",async(req,res,next)=>{
+    let {id}=req.params;
+    await Place.findByIdAndDelete(id)
+    res.redirect("/")
+})
 app.listen(3030,()=>{
     console.log("app listing on port 3030");
 })
