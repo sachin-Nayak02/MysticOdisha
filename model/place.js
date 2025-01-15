6f814ab8e971886d219f1b92d0d52f73b67d06df
Sadasiba Nayak <nayaksadasiba02@gmail.com>
2025-01-15 20:06:01 +0530

    ok

--- model/place.js
+++ model/place.js
@@ -8,30 +8,37 @@ const placeSchema=new Schema({
     },
     description:{
         type:String,
+        required:true,
     },
     image:{
         type:String,
+        required:true,
         set:(v)=> v ==="  "? "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Odisha_districts_map.svg/1200px-Odisha_districts_map.svg.png":v,
 
         default:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Odisha_districts_map.svg/1200px-Odisha_districts_map.svg.png"
     },
     imagebn:{
         type:String,
+        required:true,
         set:(v)=> v ==="  "? "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Odisha_districts_map.svg/1200px-Odisha_districts_map.svg.png":v,
 
         default:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Odisha_districts_map.svg/1200px-Odisha_districts_map.svg.png" 
     },
    dist:{
         type:String,
+        required:true,
     },
     type:{
         type:String,
+        required:true,
     },
     location:{
         type:String,
+        required:true,
     },
     bio:{
         type:String,
+        required:true,
      
     }
 });
